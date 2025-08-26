17120d396bf3e01e9ce5668795e240cb4399d857
Chivu Andrei <chivuandrei2003@gmail.com>
2025-08-26 09:49:59 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-let a;
+class Node {
+  constructor(data, leftChild, rightChild) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor(arr) {
+    this.root = this.buildTree(arr);
+  }
+
+    buildTree(arr) {
+      
+    return;
+  }
+}
